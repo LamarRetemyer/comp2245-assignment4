7c2634731566013ed77aac4bbb5175c2aa540eee
Lamar Retemyer <lamar.retemyer@gmail.com>
2023-11-12 19:04:17 -0400

    Final code

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Document loaded.');
 
-    // Listen for the click event on the "Search" button
     document.getElementById('searchButton').addEventListener('click', function () {
         console.log('Search button clicked.');
 
-        // Create a new XMLHttpRequest object
+        var searchInput = sanitizeInput(document.getElementById('searchInput').value);
         var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'superheroes.php?query=' + encodeURIComponent(searchInput), true);
 
-        // Configure it to make a GET request to superheroes.php
-        xhr.open('GET', 'superheroes.php', true);
-
-        // Set up the callback function to handle the response
         xhr.onload = function () {
             if (xhr.status >= 200 && xhr.status < 400) {
-                // On success, display the HTML response in an alert
-                alert(xhr.responseText);
+                document.getElementById('result').innerHTML = xhr.responseText;
             } else {
-                // Handle errors
                 console.error('Request failed with status:', xhr.status);
             }
         };
 
-        // Set up the callback function to handle network errors
         xhr.onerror = function () {
             console.error('Network error occurred');
         };
 
-        // Send the request
         xhr.send();
     });
 });
+
+function sanitizeInput(input) {
+    return input.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
